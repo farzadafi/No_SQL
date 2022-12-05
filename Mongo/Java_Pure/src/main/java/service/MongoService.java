ed0437fa47fee18f0c75c5cd8883b8740055e730
farzadafi <farzadafi40@gmail.com>
2022-12-05 13:46:51 +0330

    add getAllDBName method

--- Mongo/Java_Pure/src/main/java/service/MongoService.java
+++ Mongo/Java_Pure/src/main/java/service/MongoService.java
@@ -1,5 +1,7 @@
 package service;
 
+import com.mongodb.Block;
+import com.mongodb.client.MongoIterable;
 import repository.MongoRepository;
 
 import java.util.Scanner;
@@ -19,5 +21,11 @@ public class MongoService {
         String name = input.nextLine();
         mongoRepository.addDBOrSwitch(name);
         System.out.println("OK!");
+    }
+
+    public void getAllDBName(){
+        MongoIterable<String> DBName = mongoRepository.getAllDBName();
+        Block<String> printer = System.out::println;
+        DBName.forEach(printer);
     }
 }
