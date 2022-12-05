55c8cbf028fd67d2f05b26fafcab8a91c6530bc8
farzadafi <farzadafi40@gmail.com>
2022-12-05 13:41:55 +0330

    add all method for get connection to mongo server

--- Mongo/Java_Pure/src/main/java/connection/SingletonMongoDB.java
+++ Mongo/Java_Pure/src/main/java/connection/SingletonMongoDB.java
@@ -1,4 +1,38 @@
 package connection;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
 public class SingletonMongoDB {
+
+    private MongoClient mongoClient;
+    private static SingletonMongoDB obj;
+
+    private SingletonMongoDB() {
+        mongoClient = new MongoClient(
+                new MongoClientURI( "mongodb://mongo:mongo@localhost:27017" )
+        );
+    }
+
+    public static SingletonMongoDB getInstance() {
+        if (obj==null)
+            obj = new SingletonMongoDB();
+        return obj;
+    }
+
+    public MongoClient getConnection() {
+        return mongoClient;
+    }
+
+    public void setConnection(MongoClient mongoClient) {
+        this.mongoClient = mongoClient;
+    }
+
+    public static SingletonMongoDB getObj() {
+        return obj;
+    }
+
+    public static void setObj(SingletonMongoDB obj) {
+        SingletonMongoDB.obj = obj;
+    }
 }
