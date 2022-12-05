ecf8c724f104ea3a4e26ad5a6534a625162a4110
farzadafi <farzadafi40@gmail.com>
2022-12-05 13:46:34 +0330

    add addDBOrSwitch method

--- Mongo/Java_Pure/src/main/java/service/MongoService.java
+++ Mongo/Java_Pure/src/main/java/service/MongoService.java
@@ -1,4 +1,23 @@
 package service;
 
+import repository.MongoRepository;
+
+import java.util.Scanner;
+
 public class MongoService {
+
+    private final Scanner input;
+    private final MongoRepository mongoRepository;
+
+    public MongoService() {
+        input = new Scanner(System.in);
+        this.mongoRepository = new MongoRepository();
+    }
+
+    public void addDBOrSwitch(){
+        System.out.print("Enter DB name:");
+        String name = input.nextLine();
+        mongoRepository.addDBOrSwitch(name);
+        System.out.println("OK!");
+    }
 }
