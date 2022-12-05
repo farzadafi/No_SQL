760bb2648218c87c4faae8adbde0a73cbc4a9780
farzadafi <farzadafi40@gmail.com>
2022-12-05 13:43:10 +0330

    add addDBOrSwitch method

--- Mongo/Java_Pure/src/main/java/repository/MongoRepository.java
+++ Mongo/Java_Pure/src/main/java/repository/MongoRepository.java
@@ -1,4 +1,22 @@
 package repository;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import connection.SingletonMongoDB;
+
 public class MongoRepository {
+
+    private final MongoClient mongoClient;
+    private static MongoDatabase mongoDB;
+
+    public MongoRepository() {
+        this.mongoClient = SingletonMongoDB.getInstance().getConnection();
+        mongoDB = mongoClient.getDatabase("admin");
+    }
+
+    public void addDBOrSwitch(String name){
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(name);
+        mongoDB = mongoDatabase;
+        mongoDatabase.createCollection("default");
+    }
 }
