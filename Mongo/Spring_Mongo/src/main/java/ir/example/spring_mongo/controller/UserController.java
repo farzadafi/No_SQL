618e5be8aeaade012f2545062aafbfb7b7e1daa5
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:40:10 +0330

    add registerUser method

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/controller/UserController.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/controller/UserController.java
@@ -1,4 +1,28 @@
 package ir.example.spring_mongo.controller;
 
+import ir.example.spring_mongo.dto.UserDto;
+import ir.example.spring_mongo.mapper.UserMapper;
+import ir.example.spring_mongo.model.User;
+import ir.example.spring_mongo.service.user.UserServiceImpel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserServiceImpel userServiceImpel;
+
+    public UserController(UserServiceImpel userServiceImpel) {
+        this.userServiceImpel = userServiceImpel;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserDto userDto){
+        User user = UserMapper.INSTANCE.dtoToModel(userDto);
+        userServiceImpel.insert(user);
+        return "OK";
+    }
 }
