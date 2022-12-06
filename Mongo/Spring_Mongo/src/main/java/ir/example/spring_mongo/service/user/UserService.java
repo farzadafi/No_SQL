92ce4d100ae3f669eb7577d28642265ed855a281
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:28:09 +0330

    add findByEmail method

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserService.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserService.java
@@ -2,6 +2,10 @@ package ir.example.spring_mongo.service.user;
 
 import ir.example.spring_mongo.model.User;
 
+import java.util.Optional;
+
 public interface UserService {
     void insert(User user);
+
+    Optional<User> findByEmail(String email);
 }
--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserServiceImpel.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserServiceImpel.java
@@ -4,6 +4,7 @@ import ir.example.spring_mongo.model.User;
 import ir.example.spring_mongo.repository.UserRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserServiceImpel implements UserService {
 
@@ -17,5 +18,10 @@ public class UserServiceImpel implements UserService {
     public void insert(User user) {
         user.setCreated(LocalDateTime.now());
         userRepository.insert(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 }
