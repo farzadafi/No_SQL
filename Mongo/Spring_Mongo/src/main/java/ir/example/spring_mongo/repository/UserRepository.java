b7225c3296b11ff31596884347882a8ec48c7c98
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:26:40 +0330

    fix extended(from Mongo instead of Jpa)

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/repository/UserRepository.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package ir.example.spring_mongo.repository;
 
 import ir.example.spring_mongo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
 }
