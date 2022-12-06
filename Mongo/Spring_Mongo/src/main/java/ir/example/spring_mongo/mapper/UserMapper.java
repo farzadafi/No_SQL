e7e5c42b1f500686d918c9cab8e2a6a230e09ad8
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:38:09 +0330

    add dtoToModel method

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/mapper/UserMapper.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package ir.example.spring_mongo.mapper;
 
+import ir.example.spring_mongo.dto.UserDto;
+import ir.example.spring_mongo.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User dtoToModel(UserDto userDto);
 }
