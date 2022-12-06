77512ea32426b0766cea0fb7265079590883e4e9
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:26:57 +0330

    add insert method

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserService.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserService.java
@@ -1,4 +1,7 @@
 package ir.example.spring_mongo.service.user;
 
+import ir.example.spring_mongo.model.User;
+
 public interface UserService {
+    void insert(User user);
 }
--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserServiceImpel.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/service/user/UserServiceImpel.java
@@ -1,4 +1,21 @@
 package ir.example.spring_mongo.service.user;
 
+import ir.example.spring_mongo.model.User;
+import ir.example.spring_mongo.repository.UserRepository;
+
+import java.time.LocalDateTime;
+
 public class UserServiceImpel implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpel(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void insert(User user) {
+        user.setCreated(LocalDateTime.now());
+        userRepository.insert(user);
+    }
 }
