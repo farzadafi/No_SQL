004d4f8c16d6e319014a27996a0d3cc43aedfd7e
farzadafi <farzadafi40@gmail.com>
2022-12-06 15:36:04 +0330

    create Spring_Mongo module

---
+++ Mongo/Spring_Mongo/src/test/java/ir/example/spring_mongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.example.spring_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
