f21c5d6ef9993afd3f620a1f70affbfd892ef104
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:21:58 +0330

    add field same as User class

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/dto/UserDto.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/dto/UserDto.java
@@ -1,4 +1,34 @@
 package ir.example.spring_mongo.dto;
 
+import ir.example.spring_mongo.model.Address;
+import ir.example.spring_mongo.model.enumeration.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Gender gender;
+
+    private Address address;
+
+    private List<String> favouriteSubjects;
+
+    private LocalDateTime created;
 }
