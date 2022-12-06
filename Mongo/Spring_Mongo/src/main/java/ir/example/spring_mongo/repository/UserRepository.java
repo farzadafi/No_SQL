735e28bcb39cfaa3a123bc23233c6a17f45f7028
farzadafi <farzadafi40@gmail.com>
2022-12-06 20:27:55 +0330

    add findByEmail method

--- Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/repository/UserRepository.java
+++ Mongo/Spring_Mongo/src/main/java/ir/example/spring_mongo/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ir.example.spring_mongo.repository;
 import ir.example.spring_mongo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
