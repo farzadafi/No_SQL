edb0e36c4750db841618564441b8f051c8454223
farzadafi <farzadafi40@gmail.com>
2022-12-13 19:29:41 +0330

    fix constructor and connection

--- Redis/Java_pure/src/main/java/connection/JedisConnection.java
+++ Redis/Java_pure/src/main/java/connection/JedisConnection.java
@@ -1,33 +1,25 @@
 package connection;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-
-import java.time.Duration;
 
 public class JedisConnection {
 
-    private static JedisPool pool = null;
+    private static Jedis jedis = null;
 
     private static final JedisConnection jedisConnection = new JedisConnection();
 
     private JedisConnection() {
-        if (pool == null) {
+        if (jedis == null) {
             String ip = "127.0.0.1";
             int port = 6379;
-            JedisPoolConfig config = new JedisPoolConfig();
-            config.setMaxTotal(10000);
-            config.setMaxIdle(2000);
-            config.setMaxWait(Duration.ofDays(1));
-            config.setTestOnBorrow(true);
-            pool = new JedisPool(config, ip, port, 100000);
+            //JedisPoolConfig config = new JedisPoolConfig();
+            jedis = new Jedis(String.format("redis://%s:%s", ip, port));
         }
 
     }
 
     public Jedis getJedis() {
-        return pool.getResource();
+        return jedis;
     }
 
     public static JedisConnection getInstance() {
