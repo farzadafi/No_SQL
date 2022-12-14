9941503abf921b65cfa078d99efb1653c73ced1d
farzadafi <farzadafi40@gmail.com>
2022-12-14 09:20:27 +0330

    add register method

--- Redis/Spring_Redis/src/main/java/com/example/spring/service/UserService.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.spring.service;
 
+import com.example.spring.model.User;
+
 public interface UserService {
+    void register(User user);
 }
--- Redis/Spring_Redis/src/main/java/com/example/spring/service/UserServiceImpel.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/service/UserServiceImpel.java
@@ -1,4 +1,18 @@
 package com.example.spring.service;
 
+import com.example.spring.model.User;
+import com.example.spring.repository.UserRepository;
+
 public class UserServiceImpel implements UserService{
+
+    private final UserRepository repository;
+
+    public UserServiceImpel(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void register(User user) {
+        repository.save(user);
+    }
 }
