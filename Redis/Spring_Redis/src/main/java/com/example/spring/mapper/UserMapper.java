02a70a49b6a02d8dc262e09d19d5c001b41a90e2
farzadafi <farzadafi40@gmail.com>
2022-12-14 09:05:05 +0330

    add dtoToModel class

--- Redis/Spring_Redis/src/main/java/com/example/spring/mapper/UserMapper.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.example.spring.mapper;
 
+import com.example.spring.dto.UserDto;
+import com.example.spring.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User dtoToModel(UserDto userDto);
 }
