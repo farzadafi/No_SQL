78d7639d074c567d4b2f7f1039202a82f83b1a82
farzadafi <farzadafi40@gmail.com>
2022-12-14 09:04:55 +0330

    add some field like as User class

--- Redis/Spring_Redis/src/main/java/com/example/spring/dto/UserDto.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/dto/UserDto.java
@@ -1,4 +1,21 @@
 package com.example.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String username;
 }
