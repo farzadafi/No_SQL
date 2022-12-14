1eac75c478f0123681d4a71085e4fce72b536474
farzadafi <farzadafi40@gmail.com>
2022-12-14 08:53:48 +0330

    add some field and redisHash annotation

--- Redis/Spring_Redis/src/main/java/com/example/spring/model/User.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/model/User.java
@@ -1,4 +1,26 @@
 package com.example.spring.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Entity
+@RedisHash("user")
 public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String username;
 }
