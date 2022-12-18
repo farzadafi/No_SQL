df21d97a06e0748dc79195e2db15cb55f5b481d0
farzadafi <farzadafi40@gmail.com>
2022-12-18 18:25:22 +0330

    extended from RuntimeException and implements one constructor

--- Redis/Spring_Redis/src/main/java/com/example/spring/exception/NotFoundException.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/exception/NotFoundException.java
@@ -1,4 +1,8 @@
 package com.example.spring.exception;
 
-public class NotFoundException {
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
