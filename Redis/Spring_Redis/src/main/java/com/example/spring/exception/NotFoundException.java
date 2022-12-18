44d593dbb77489b1a926d96c2b436b6b75cffe9b
farzadafi <farzadafi40@gmail.com>
2022-12-18 18:26:22 +0330

    add @ResponseStatus

--- Redis/Spring_Redis/src/main/java/com/example/spring/exception/NotFoundException.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/exception/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.spring.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Unfortunately not found!")
 public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
