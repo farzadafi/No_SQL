d0c7899ee6901b0a5e4e2b68815654757a5615f5
farzadafi <farzadafi40@gmail.com>
2022-12-18 18:21:09 +0330

    add registerUser method

--- Redis/Spring_Redis/src/main/java/com/example/spring/controller/UserController.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.example.spring.controller;
 
+import com.example.spring.dto.UserDto;
+import com.example.spring.mapper.UserMapper;
+import com.example.spring.model.User;
+import com.example.spring.service.UserServiceImpel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserServiceImpel userService;
+
+    public UserController(UserServiceImpel userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/registerUser")
+    public String registerUser(@RequestBody UserDto userDto) {
+        User user = UserMapper.INSTANCE.dtoToModel(userDto);
+        userService.register(user);
+        return "OK";
+    }
 }
