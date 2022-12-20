708ad9eab573665289f772ff67eaa4ebad2ab904
farzadafi <farzadafi40@gmail.com>
2022-12-20 06:31:25 +0330

    change id type from Integer to String

--- Redis/Spring_Redis/src/main/java/com/example/spring/repository/UserRepository.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, String> {
     Optional<User> findByUsername(String username);
 }
