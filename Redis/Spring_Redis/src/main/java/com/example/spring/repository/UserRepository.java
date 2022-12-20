69402f68edfa5427e3134608209f67ff3f041c31
farzadafi <farzadafi40@gmail.com>
2022-12-20 06:15:45 +0330

    add findByUsername method

--- Redis/Spring_Redis/src/main/java/com/example/spring/repository/UserRepository.java
+++ Redis/Spring_Redis/src/main/java/com/example/spring/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.spring.repository;
 import com.example.spring.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
